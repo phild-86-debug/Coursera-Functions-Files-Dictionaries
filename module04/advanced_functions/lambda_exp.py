acb95181ca053de15605377b5f11a1d173d64183
phild-86-debug <phildexter86@gmail.com>
2024-10-18 18:35:40 -0400

    Added advanced_functions module

---
+++ module04/advanced_functions/lambda_exp.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 17 05:11:43 2024
+
+@author: phild
+"""
+
+pairs = [(1, 'one'), (2, 'two'), (3, 'three', ), (4, 'four')]
+pairs.sort()
+# pairs.sort(key=lambda pair: pair[1])
+print(pairs)
+
